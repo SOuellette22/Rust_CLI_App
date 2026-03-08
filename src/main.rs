c33740d80984b26e43606b71f2247dfe9a28d3d4
SOuellette22 <ouellettese@merrimack.edu>
2026-03-07 20:32:05 -0500

    Docs: commented the task.rs file

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use crate::task::Task;
 mod task;
 mod task_map;
 
-const file_path: &str = "db/task.json";
+const FILE_PATH: &str = "db/task.json";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
@@ -22,7 +22,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
 
     let mut task_map = TaskMap::new();
-    task_map.load(file_path)?;
+    task_map.load(FILE_PATH)?;
     task_map.add_task(task1);
     task_map.add_task(task2);
 
@@ -34,7 +34,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Task: {}, Due: {:?}, Completed: {}", name, task.get_due_date(), task.is_completed());
     }
 
-    task_map.save(file_path)?;
+    task_map.save(FILE_PATH)?;
 
     Ok(())
 
--- src/task.rs
+++ src/task.rs
@@ -1,6 +1,7 @@
 use chrono::{NaiveDate};
 use serde::{Deserialize, Serialize};
 
+/// Represents a task with a name, optional subject, optional due date, and completion status.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Task {
     name: String,
@@ -9,8 +10,10 @@ pub struct Task {
     completed: bool
 }
 
+/// Implementation of the Task struct, including constructors, getters, and helper functions.
 impl Task {
 
+    /// Creates a new Task with the given name, optional subject, and optional due date. The task is initially marked as incomplete.
     pub fn new(name: String, subject: Option<String>, due_date: Option<NaiveDate>) -> Self {
         Task {
             name,
@@ -22,28 +25,34 @@ impl Task {
 
     // ---- Getters ---- //
 
+    /// Returns the name of the task.
     pub fn get_name(&self) -> String {
         self.name.clone()
     }
 
+    /// Returns the subject of the task, if it exists.
     pub fn get_subject(&self) -> Option<String> {
         self.subject.clone()
     }
 
+    /// Returns the due date of the task, if it exists.
     pub fn get_due_date(&self) -> Option<NaiveDate> {
         self.due_date
     }
 
+    /// Returns whether the task is completed.
     pub fn is_completed(&self) -> bool {
         self.completed
     }
 
     // ---- Helper Functions ---- //
 
+    /// Marks the task as completed.
     pub fn mark_completed(&mut self) {
         self.completed = true;
     }
 
+    /// Marks the task as incomplete.
     pub fn mark_incomplete(&mut self) {
         self.completed = false;
     }
